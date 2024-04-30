487a39ac7f6fc7665b0edf87e492e6b658f9340b
Austyn Whaley <austyn_whaley@yahoo.com>
2024-04-30 03:02:05 -0400

    backend running

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 "use strict";
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-mongoose.connect('mongodb://localhost:27017/d3db');
-const UserSchema = new mongoose.Schema({
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+mongoose_1.default.connect('mongodb://localhost:27017/d3db').then(() => {
+    console.log("Connected to db");
+}).catch((err) => {
+    console.log(err);
+});
+const UserSchema = new mongoose_1.default.Schema({
     name: String,
     email: String,
 });
-const UserModel = mongoose.model("users", UserSchema);
-app.get("/getUsers", (res) => {
+const UserModel = mongoose_1.default.model("users", UserSchema);
+// Enable CORS for all routes
+app.use((0, cors_1.default)());
+app.get("/users", (req, res) => {
     UserModel.find({}).then((users) => {
+        console.log('req', req);
         res.json(users);
     }).catch((err) => {
-        console.log(err);
+        console.log('ahhh', err);
+        res.status(500).json({ error: "An error occurred while fetching users." });
     });
 });
-app.listen(4000, () => {
-    console.log('App running on port 4000');
+app.get("/", (req, res) => {
+    console.log('BING');
+    res.send('Hello World!');
+});
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,25 +1,41 @@
-const express = require('express');
-const mongoose = require('mongoose');
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/d3db')
+mongoose.connect('mongodb://localhost:27017/d3db').then(() => {
+    console.log("Connected to db")
+}).catch((err) => {
+    console.log(err);
+});
 
 const UserSchema = new mongoose.Schema({
     name: String,
     email: String,
 });
 
-const UserModel = mongoose.model("users", UserSchema)
+const UserModel = mongoose.model("users", UserSchema);
 
-app.get("/getUsers", (res: any) => {
-    UserModel.find({}).then((users: any) => {
-        res.json(users)
-    }).catch((err: any) => {
-        console.log(err);
-    })
+// Enable CORS for all routes
+app.use(cors());
+
+app.get("/users", (req: Request, res: Response) => {
+    UserModel.find({}).then((users) => {
+        res.json(users);
+    }).catch((err) => {
+        console.log('ahhh',err);
+        res.status(500).json({ error: "An error occurred while fetching users." });
+    });
 });
 
-app.listen(4000, () => {
-    console.log('App running on port 4000');
+app.get("/", (req: Request, res: Response) => {
+    console.log('BING');
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}`);
 });
